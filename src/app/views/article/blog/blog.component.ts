ae393941c26da9d5d3a43a06000e65218de34e60
esukhova <e_ukhova@inbox.ru>
2025-12-08 23:19:33 +0300

    add card-loaders to blog

--- src/app/views/article/blog/blog.component.ts
+++ src/app/views/article/blog/blog.component.ts
@@ -17,114 +17,120 @@ import {debounceTime, Subscription} from "rxjs";
 })
 export class BlogComponent implements OnInit, OnDestroy {
 
-    articles: ArticleType[] = [];
-    categories: CategoryType[] = [];
-    filterOpen = false;
-    activeParams: ActiveParamsType = {categories: []};
-    appliedFilters: AppliedFilterType[] = [];
-    pages: number[] = [];
-    private _subscriptions: Subscription = new Subscription();
+  showLabel: boolean = false;
+  articles: ArticleType[] = [];
+  categories: CategoryType[] = [];
+  filterOpen = false;
+  activeParams: ActiveParamsType = {categories: []};
+  appliedFilters: AppliedFilterType[] = [];
+  pages: number[] = [];
+  private _subscriptions: Subscription = new Subscription();
 
-    constructor(private articleService: ArticleService,
-                private categoryService: CategoryService,
-                private router: Router,
-                private activatedRoute: ActivatedRoute) {
-    }
+  constructor(private articleService: ArticleService,
+              private categoryService: CategoryService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
 
-    ngOnInit(): void {
-      this._subscriptions.add(this.categoryService.getCategories()
-            .subscribe((data: CategoryType[]) => {
-                this.categories = data;
+  ngOnInit(): void {
+    this._subscriptions.add(this.categoryService.getCategories()
+      .subscribe((data: CategoryType[]) => {
+        this.categories = data;
 
-              this._subscriptions.add(this.activatedRoute.queryParams
-                    .pipe(
-                        debounceTime(500)
-                    )
-                    .subscribe(params => {
-                    this.activeParams = ActiveParamsUtil.processParams(params);
+        this._subscriptions.add(this.activatedRoute.queryParams
+          .pipe(
+            debounceTime(500)
+          )
+          .subscribe(params => {
+            this.activeParams = ActiveParamsUtil.processParams(params);
 
-                    this.appliedFilters = [];
-                    this.activeParams.categories.forEach(url => {
-                        const foundCategory = this.categories.find(category => category.url === url)
-                        if (foundCategory) {
-                            this.appliedFilters.push({
-                                name: foundCategory.name,
-                                urlParam: foundCategory.url
-                            })
-                        }
-                    })
-
-                      this._subscriptions.add(this.articleService.getArticles(this.activeParams)
-                        .subscribe(data => {
-                            this.pages = [];
-                            for (let i = 1; i <= data.pages; i++) {
-                                this.pages.push(i);
-                            }
-
-                            if (data.items && data.items.length > 0) {
-                                this.articles = data.items;
-                            } else {
-                                this.articles = [];
-                            }
-                        }));
-                }))
-            }))
-    }
-
-    toggleFilterList() {
-        this.filterOpen = !this.filterOpen;
-    }
-
-    updateFilterParam(categoryUrl: string, checkedFilter: boolean) {
-
-        if (this.activeParams.categories && this.activeParams.categories.length > 0) {
-            const existingCategoryInParams = this.activeParams.categories.find(item => item === categoryUrl);
-            if (existingCategoryInParams && !checkedFilter) {
-                this.activeParams.categories = this.activeParams.categories.filter(item => item !== categoryUrl);
-            } else if (!existingCategoryInParams && checkedFilter) {
-                this.activeParams.categories = [...this.activeParams.categories, categoryUrl];
-            }
-        } else if (checkedFilter) {
-            this.activeParams.categories = [categoryUrl];
-        }
-        this.activeParams.page = 1;
-        this.router.navigate(['/blog'], {
-            queryParams: this.activeParams
-        })
-    }
-
-    removeAppliedFilter(appliedFilter: AppliedFilterType) {
-        this.activeParams.categories = this.activeParams.categories.filter(category => category !== appliedFilter.urlParam);
-        this.activeParams.page = 1;
-        this.router.navigate(['/blog'], {
-            queryParams: this.activeParams
-        })
-    }
-
-    openPage(page: number) {
-        this.activeParams.page = page;
-        this.router.navigate(['/blog'], {
-            queryParams: this.activeParams
-        })
-    }
-
-    openPrevPage() {
-        if (this.activeParams.page && this.activeParams.page > 1) {
-            this.activeParams.page--;
-            this.router.navigate(['/blog'], {
-                queryParams: this.activeParams
+            this.appliedFilters = [];
+            this.activeParams.categories.forEach(url => {
+              const foundCategory = this.categories.find(category => category.url === url)
+              if (foundCategory) {
+                this.appliedFilters.push({
+                  name: foundCategory.name,
+                  urlParam: foundCategory.url
+                })
+              }
             })
-        }
-    }
 
-    openNextPage() {
-        if (this.activeParams.page && this.activeParams.page < this.pages.length) {
-            this.activeParams.page++;
-            this.router.navigate(['/blog'], {
-                queryParams: this.activeParams
-            })
-        }
+            this._subscriptions.add(this.articleService.getArticles(this.activeParams)
+              .subscribe(data => {
+                this.pages = [];
+                for (let i = 1; i <= data.pages; i++) {
+                  this.pages.push(i);
+                }
+
+                if (data.items && data.items.length > 0) {
+                  this.articles = data.items;
+                  this.showLabel = false;
+                } else if (data.items && data.items.length === 0) {
+                  this.articles = [];
+                  this.showLabel = true;
+                } else {
+                  this.articles = [];
+                  this.showLabel = true;
+                }
+              }));
+          }))
+      }))
+  }
+
+  toggleFilterList() {
+    this.filterOpen = !this.filterOpen;
+  }
+
+  updateFilterParam(categoryUrl: string, checkedFilter: boolean) {
+
+    if (this.activeParams.categories && this.activeParams.categories.length > 0) {
+      const existingCategoryInParams = this.activeParams.categories.find(item => item === categoryUrl);
+      if (existingCategoryInParams && !checkedFilter) {
+        this.activeParams.categories = this.activeParams.categories.filter(item => item !== categoryUrl);
+      } else if (!existingCategoryInParams && checkedFilter) {
+        this.activeParams.categories = [...this.activeParams.categories, categoryUrl];
+      }
+    } else if (checkedFilter) {
+      this.activeParams.categories = [categoryUrl];
     }
+    this.activeParams.page = 1;
+    this.router.navigate(['/blog'], {
+      queryParams: this.activeParams
+    })
+  }
+
+  removeAppliedFilter(appliedFilter: AppliedFilterType) {
+    this.activeParams.categories = this.activeParams.categories.filter(category => category !== appliedFilter.urlParam);
+    this.activeParams.page = 1;
+    this.router.navigate(['/blog'], {
+      queryParams: this.activeParams
+    })
+  }
+
+  openPage(page: number) {
+    this.activeParams.page = page;
+    this.router.navigate(['/blog'], {
+      queryParams: this.activeParams
+    })
+  }
+
+  openPrevPage() {
+    if (this.activeParams.page && this.activeParams.page > 1) {
+      this.activeParams.page--;
+      this.router.navigate(['/blog'], {
+        queryParams: this.activeParams
+      })
+    }
+  }
+
+  openNextPage() {
+    if (this.activeParams.page && this.activeParams.page < this.pages.length) {
+      this.activeParams.page++;
+      this.router.navigate(['/blog'], {
+        queryParams: this.activeParams
+      })
+    }
+  }
 
   ngOnDestroy() {
     this._subscriptions.unsubscribe();
